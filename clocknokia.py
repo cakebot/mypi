9dbc80407c45c3f3e01462aef0c211abd10dc295
cakebot <jim.baur@gmail.com>
2014-10-29 03:48:18 +0000

    Added buzzer and switch for alarm function.

--- clocknokia.py
+++ clocknokia.py
@@ -25,10 +25,25 @@ SPI_PORT = 0
 SPI_DEVICE = 0
 
 # GPIO setup
+exitB = 17
+leftB = 22
+rightB = 26
+okB = 13
+alarmS = 16
+buzzer = 12
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(17, GPIO.IN)
+GPIO.setwarnings(False)
+GPIO.setup(exitB, GPIO.IN)
+GPIO.setup(leftB, GPIO.IN)
+GPIO.setup(rightB, GPIO.IN)
+GPIO.setup(okB, GPIO.IN)
+GPIO.setup(alarmS, GPIO.IN)
+GPIO.setup(buzzer, GPIO.OUT)
+
 
 def setup_screen():
+	# turn buzzer off by default
+	GPIO.output(buzzer, False)
 	# Hardware SPI usage:
 	disp = LCD.PCD8544(DC, RST, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=4000000))
 
@@ -73,13 +88,19 @@ def main():
 		curdate = strftime("%b %d, %Y")
 		disp.clear()
 		draw.rectangle((0, 0, LCD.LCDWIDTH, LCD.LCDHEIGHT), outline=255, fill=255)
+		# alarm check
+		if(GPIO.input(alarmS) == False):
+			draw.text((30, 32), "X", font=defaultfont)
+			GPIO.output(buzzer, True)
+		else:
+			GPIO.output(buzzer, False)
 		draw.text((1, 0), curtime, font=defaultfont)
 		draw.text((1, 8), weekday, font=defaultfont)
 		draw.text((1, 16), curdate, font=defaultfont)
 		disp.image(image)
 		disp.display()
 		# exits script if button is pressed
-		if (GPIO.input(17) == False):
+		if (GPIO.input(exitB) == False):
 			kill_clock(disp, image, draw)
 
 
@@ -91,4 +112,20 @@ def kill_clock(disp, image, draw):
 	exit(0)		
 main()
 
+
+
+'''
+NOTES FOR FUTURE BUILDS
+-----------------------
+
+* time zone setting
+* alarm with motion sensor snooze
+* moton sensor backlight trigger
+* musical alarms
+* rising volume alarm
+* snooze length set
+* kill alarm after timeout period
+* weather
+
+''' 
 
